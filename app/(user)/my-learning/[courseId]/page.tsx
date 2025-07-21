85e2134d2aafb6a34061df1e2107ffd9e2a415f8
Vance-sama <67249759+VansRouges@users.noreply.github.com>
2025-07-21 08:53:01 +0100

    fix: course enrollment and my learning now works

--- app/(user)/my-learning/[courseId]/page.tsx
+++ app/(user)/my-learning/[courseId]/page.tsx
@@ -18,7 +18,7 @@ export default async function CoursePage({ params }: CoursePageProps) {
   // Redirect to the first lesson of the first module if available
   if (course.modules?.[0]?.lessons?.[0]?._id) {
     return redirect(
-      `/dashboard/courses/${courseId}/lessons/${course.modules[0].lessons[0]._id}`
+      `/my-learning/${courseId}/lessons/${course.modules[0].lessons[0]._id}`
     );
   }
 
